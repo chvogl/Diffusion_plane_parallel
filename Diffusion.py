0bbb08c8bc67ec89914113d6a1291823e5e6a9de
Christian Vogl <cvogl@mpa-garching.mpg.de>
2015-05-06 14:49:14 +0200

    -corrected error in unit conversion: previous version used   speed of light in units 100m/s instead of cm/s -added plotting of theoretical radiation field energy density   and plotting of the radiation field energy density of the MC-   simulation (previous version only plots number of photon   packets)

--- Diffusion.py
+++ Diffusion.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import constants
 
-c = 0.01 * constants.c  # in cm s^-1
+c = 100 * constants.c  # in cm s^-1
 xi = 100.
 
 
@@ -20,35 +20,56 @@ class Packet:
         self.mu = 2 * np.random.random() - 1
         self.ltot += l_free
 
-    def getltot(self):
-        return self.ltot
-
-    def gety(self):
-        return self.y
-
 
 class Experiment:
     def __init__(self, E, N):
         self.E = E
         self.N = N
 
-    def getEpacket(self):
-        return self.E / float(self.N)
-
     def Radfield_at_time(self, t):
         ylist = np.zeros(self.N)
         epacket = self.E / float(self.N)
         for i in xrange(self.N):
             Testpacket = Packet(epacket)
-            while Testpacket.getltot() / c <= t:
+            while Testpacket.ltot / c <= t:
                 Testpacket.step()
-            ylist[i] = Testpacket.gety()
+            ylist[i] = Testpacket.y
         return ylist
 
+    def TheoryPlot(self, t, plotcolor):
+        """Plots theoretical prediction for the radiation
+         field energy density at time t"""
+        E = self.E
+        D = c / (3 * xi)
+        x = np.linspace(-0.5, 0.5, num=100)
+        E_density = E * np.exp(-x ** 2 / (4 * D * t)) / np.sqrt(4 * np.pi * D * t)
+        plt.plot(x, E_density, color=plotcolor)
+        plt.ylabel('E [erg cm^-1]')
+        plt.xlabel('x [cm]')
+        plt.axis([-0.5, 0.5, 0, 1.4E9])
 
-Exp1 = Experiment(10 ** 8, 10000)
-Exp1list = Exp1.Radfield_at_time(5 * 10 ** (-12))
-plt.hist(Exp1list, 50, normed=1, facecolor='g', alpha=0.75, histtype='step')
+    def MCplot(self, t, no_bins, plotcolor):
+        """Plots result of Monte Carlo simulation of the
+        radiation field energy density at time t"""
+        Explist = self.Radfield_at_time(t)
+        weightslist = [self.E * no_bins / float(self.N) for i in range(len(Explist))]
+        binwidth = 1. / no_bins
+        bins1 = np.arange(-0.5, 0.5 + binwidth, binwidth)
+        plt.hist(Explist, bins=bins1, weights=weightslist, color=plotcolor, facecolor='g', alpha=0.75, histtype='step')
+        plt.ylabel('E [erg cm^-1]')
+        plt.xlabel('x [cm]')
+        plt.axis([-0.5, 0.5, 0, 1.4E9])
+
+
+Exp1 = Experiment(10 ** 8, 10 ** 5)
+Exp1.MCplot(5 * 10 ** (-12), 200, 'r')
+Exp1.MCplot(10 * 10 ** (-12), 200, 'b')
+Exp1.MCplot(20 * 10 ** (-12), 200, 'g')
+Exp1.MCplot(50 * 10 ** (-12), 200, 'y')
+Exp1.TheoryPlot(5 * 10 ** (-12), 'r')
+Exp1.TheoryPlot(10 * 10 ** (-12), 'b')
+Exp1.TheoryPlot(20 * 10 ** (-12), 'g')
+Exp1.TheoryPlot(50 * 10 ** (-12), 'y')
 plt.show()
 
 
